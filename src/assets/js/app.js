2f332796afd883d0637ab8ba7df71450b3ae8227
evgeni <kluev.evga@gmail.com>
2020-11-28 19:25:18 +0300

    update assets & slider & other sections

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,56 +1,71 @@
 "use strict"
-
-// const burger = document.querySelector(".burger");
-// const header = document.querySelector(".header");
-// const nav = document.querySelector(".header nav");
-// const menuOpen = document.querySelector(".menu-open");
-// const menuClose = document.querySelector(".menu-close");
-
-// burger.addEventListener("click", () => {
-//     header.classList.toggle("open");
-//     nav.classList.toggle("animate__slideInDown");
-//     nav.classList.toggle("animate__slideOutUp");
-// })
-
-
 import Glide from '@glidejs/glide'
 
-new Glide('.glide', {
-    type: 'slider',
-    startAt: 0,
-    perView: 5,
-    focusAt: 0,
-    gap: 40,
-    autoplay: 5000,
-    hoverpause: true,
-    keyboard: true,
-    bound: true,
-    swipeThreshold: 80,
-    dragThreshold: 120,
-    perTouch: 1,
-    touchAngle: 45,
-    animationDuration: 350,
-    rewind: false,
-    breakpoints: {
-        800: {
-            perView: 2
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const hamburger = document.querySelector(".hamburger");
+    const header = document.querySelector(".header");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("is-active");
+        header.classList.toggle("menu_open");
+    })
+
+    new Glide('.glide', {
+        type: 'slider',
+        startAt: 0,
+        perView: 5,
+        focusAt: 0,
+        gap: 40,
+        autoplay: 5000,
+        hoverpause: true,
+        keyboard: true,
+        bound: true,
+        swipeThreshold: 50,
+        dragThreshold: 100,
+        touchAngle: 45,
+        animationDuration: 350,
+        rewind: false,
+        breakpoints: {
+            1580: {
+                perView: 4
+            },
+            1280: {
+                perView: 3
+            },
+            992: {
+                perView: 2.5
+            },
+            768: {
+                perView: 2
+            },
+            640: {
+                perView: 2
+            },
+            480: {
+                perView: 1.5
+            }
         }
-    }
-}).mount();
+    }).mount();
 
 
-new Glide('.glide-carousel', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 1,
-    focusAt: 0,
-    gap: 0,
-    autoplay: 5000,
-    hoverpause: true,
-    keyboard: true,
-    swipeThreshold: 80,
-    dragThreshold: 120,
-    perTouch: 1,
-    touchAngle: 45,
-    animationDuration: 350,
-}).mount();
+    new Glide('.glide-carousel', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 1,
+        focusAt: 0,
+        gap: 0,
+        autoplay: 5000,
+        hoverpause: true,
+        keyboard: true,
+        swipeThreshold: 80,
+        dragThreshold: 120,
+        perTouch: 1,
+        touchAngle: 45,
+        animationDuration: 350,
+    }).mount();
+
+
+
+})
