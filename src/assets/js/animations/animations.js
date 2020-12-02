b35c69a42ce03cb240168fe8c241ab140f910317
evgeni <kluev.evga@gmail.com>
2020-12-02 20:05:07 +0300

    js refactoring

--- src/assets/js/animations/animations.js
+++ src/assets/js/animations/animations.js
@@ -1,71 +1,69 @@
-"use strict"
-
 export default function() {
 
-    ///////////////////////////////////////////////////////////////////_____PREVIEW_HEADER_ANIMATION
-    const text = document.querySelector("h1");
-    const strText = text.textContent;
-    const splitText = strText.split("");
-    text.textContent = "";
+  ///////////////////////////////////////////////////////////////////_____PREVIEW_HEADER_ANIMATION
+  const text = document.querySelector("h1");
+  text.textContent = "";
+  const splitText = 'Listen To The beat Of The Web'.split("");
 
-    for (let i = 0; i < splitText.length; i++) {
-        text.innerHTML += `<span style="display:inline-block; --i:${i*0.025}s" class="slide-bottom">${splitText[i]}</span>`;
+  for (let i = 0; i < splitText.length; i++) {
+    const margin = (splitText[i + 1] == ' ') ? 'margin-right: 0.2em;' : '';
+    text.innerHTML += `<span style="display:inline-block; --i:${(i*0.025).toFixed(3)}s; ${margin}" class="slide-bottom">${splitText[i]}</span>`;
+  }
+
+
+  ///////////////////////////////////////////////////////////////////_____PREVIEW_CARDS_ANIMATION
+  document.querySelectorAll(".preview__card").forEach(item => {
+    let x, y;
+
+    function onmousemove(e) {
+      let X = e.clientX - x + "px";
+      let Y = e.clientY - y + "px";
+      item.style = `background: radial-gradient(ellipse at top ${Y} left ${X}, #fff7 10%, #fff4 40%, #fff2);`
     }
 
-
-    ///////////////////////////////////////////////////////////////////_____PREVIEW_CARDS_ANIMATION
-    document.querySelectorAll(".preview__card").forEach(item => {
-        let x, y;
-
-        function onmousemove(e) {
-            let X = e.clientX - x + "px";
-            let Y = e.clientY - y + "px";
-            item.style = `background: radial-gradient(ellipse at top ${Y} left ${X}, #fff7 10%, #fff4 40%, #fff2);`
-        }
-
-        item.addEventListener("mouseenter", function() {
-            let rect = this.getBoundingClientRect();
-            x = rect.left;
-            y = rect.top;
-            this.addEventListener("mousemove", onmousemove);
-        })
+    item.addEventListener("mouseenter", function() {
+      let rect = this.getBoundingClientRect();
+      x = rect.left;
+      y = rect.top;
+      this.addEventListener("mousemove", onmousemove);
     })
+  })
 
 
-    ///////////////////////////////////////////////////////////////////_____TESTIMONIAL_CARDS_ANIMATION
-    const testiomonialHeader = document.querySelector(".testimonial h2");
-    testiomonialHeader.classList.add("wow", "animate__animated", "animate__bounceInLeft");
-    testiomonialHeader.setAttribute('data-wow-delay', "0.3s");
+  ///////////////////////////////////////////////////////////////////_____TESTIMONIAL_CARDS_ANIMATION
+  const testiomonialHeader = document.querySelector(".testimonial h2");
+  testiomonialHeader.classList.add("wow", "animate__animated", "animate__fadeInLeft");
+  testiomonialHeader.setAttribute('data-wow-delay', "0.3s");
 
 
-    const testiomonialCards = document.querySelectorAll(".testimonial__card");
-    let cardsDelay = 0.2;
-    testiomonialCards.forEach((item) => {
-        item.classList.add("wow", "animate__animated", "animate__bounceInLeft");
-        item.setAttribute('data-wow-delay', `${cardsDelay}s`);
-        cardsDelay = cardsDelay - 0.1;
-    })
+  const testiomonialCards = document.querySelectorAll(".testimonial__card");
+  let cardsDelay = 0.2;
+  testiomonialCards.forEach((item) => {
+    item.classList.add("wow", "animate__animated", "animate__fadeInLeft");
+    item.setAttribute('data-wow-delay', `${cardsDelay}s`);
+    cardsDelay = cardsDelay - 0.1;
+  })
 
 
-    ///////////////////////////////////////////////////////////////////_____FEATURES_CARDS_ANIMATION
-    const featuresHeader = document.querySelector(".features h2");
-    featuresHeader.classList.add("wow", "animate__animated", "animate__zoomIn", "animate__faster");
+  ///////////////////////////////////////////////////////////////////_____FEATURES_CARDS_ANIMATION
+  const featuresHeader = document.querySelector(".features h2");
+  featuresHeader.classList.add("wow", "animate__animated", "animate__fadeInDown", "animate__faster");
 
 
-    const featuresCards = document.querySelectorAll(".features__card");
-    let features = 0.1;
-    featuresCards.forEach((item) => {
-        item.classList.add("wow", "animate__animated", "animate__zoomIn", "animate__faster");
-        item.setAttribute('data-wow-delay', `${features}s`);
-        features = features + 0.1;
-    })
+  const featuresCards = document.querySelectorAll(".features__card");
+  let features = 0.1;
+  featuresCards.forEach((item) => {
+    item.classList.add("wow", "animate__animated", "animate__fadeInDown", "animate__faster");
+    item.setAttribute('data-wow-delay', `${features}s`);
+    features = features + 0.05;
+  })
 
 
-    ///////////////////////////////////////////////////////////////////_____TEAM_CARDS_ANIMATION
-    const teamRow = document.querySelector(".team__row");
-    teamRow.classList.add("wow", "animate__animated", "animate__slideInUp");
+  ///////////////////////////////////////////////////////////////////_____TEAM_CARDS_ANIMATION
+  const teamRow = document.querySelector(".team__row");
+  teamRow.classList.add("wow", "animate__animated", "animate__slideInUp");
 
-    const teamCards = document.querySelectorAll(".team__card");
-    teamCards.forEach((item) => item.classList.add("wow", "animate__animated", "animate__slideInUp"))
+  const teamCards = document.querySelectorAll(".team__card");
+  teamCards.forEach((item) => item.classList.add("wow", "animate__animated", "animate__slideInUp"))
 
 }
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,3 @@
-"use strict"
-
 import Glide from '@glidejs/glide'
 import WOW from "wow.js/dist/wow.min.js";
 
@@ -13,105 +11,101 @@ import featuresCard from "./cards/featuresCard";
 import teamCard from "./cards/teamCard";
 
 import {
-    previewCardsData,
-    testimonialCardsData,
-    featuresCardsData,
-    teamCardsData
+  previewCardsData,
+  testimonialCardsData,
+  featuresCardsData,
+  teamCardsData
 } from "./cards/data";
 
 
 
 import Slider from "./sliders/constructor";
 import {
-    sliderHTML,
-    carouselHTML
+  sliderHTML,
+  carouselHTML
 } from "./sliders/constructor";
 import {
-    sliderProps,
-    carouselProps,
-    sliderData,
-    carouselData,
+  sliderProps,
+  carouselProps,
+  sliderData,
+  carouselData,
 } from "./sliders/data";
 
 import addAnimations from "./animations/animations";
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Section(parentSelector, cardClassName, innerHTMLfunction, contentArray)
+  new Section(".preview__cards", "preview__card", previewCard, previewCardsData).render();
+  new Section(".testimonial__cards", "testimonial__card", testimonialCard, testimonialCardsData).render();
+  new Section(".features__cards", "features__card", featuresCard, featuresCardsData).render();
+  new Section(".team__cards", "team__card", teamCard, teamCardsData).render();
 
-    // Section(parentSelector, cardClassName, innerHTMLfunction, contentArray)
-    new Section(".preview__cards", "preview__card", previewCard, previewCardsData).render();
-    new Section(".testimonial__cards", "testimonial__card", testimonialCard, testimonialCardsData).render();
-    new Section(".features__cards", "features__card", featuresCard, featuresCardsData).render();
-    new Section(".team__cards", "team__card", teamCard, teamCardsData).render();
+  // Section(parentSelector, innerHTMLfunction, contentArray)
+  new Slider(".glide", sliderHTML, sliderData).render();
+  new Slider(".glide-carousel", carouselHTML, carouselData).render();
 
-    // Section(parentSelector, innerHTMLfunction, contentArray)
-    new Slider(".glide", sliderHTML, sliderData).render();
-    new Slider(".glide-carousel", carouselHTML, carouselData).render();
-
-    new Glide(".glide", sliderProps).mount();
-    new Glide(".glide-carousel", carouselProps).mount();
+  new Glide(".glide", sliderProps).mount();
+  new Glide(".glide-carousel", carouselProps).mount();
 
 
-    addAnimations();
+  addAnimations();
 
-    new WOW().init();
+  new WOW().init();
 
-    ///////////////////////////////////////////////////////////////////_____BURGER
-    const hamburger = document.querySelector(".hamburger");
+  ///////////////////////////////////////////////////////////////////_____BURGER
+  document.querySelector(".hamburger").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+    document.querySelector(".header").classList.toggle("menu_open");
+  })
+
+  ///////////////////////////////////////////////////////////////////_____Observer
+
+  const menuToggler = target => {
     const header = document.querySelector(".header");
+    const previewCardsSection = document.querySelector(".preview__cards-section");
+    const testimonial = document.querySelector(".testimonial");
+    const slider = document.querySelector(".slider");
+    const features = document.querySelector(".features");
+    const carousel = document.querySelector(".carousel");
+    const team = document.querySelector(".team");
 
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("is-active");
-        header.classList.toggle("menu_open");
-    })
+    const io = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
 
-    ///////////////////////////////////////////////////////////////////_____Observer
-
-    const menuToggler = target => {
-        const header = document.querySelector(".header");
-        const previewCardsSection = document.querySelector(".preview__cards-section");
-        const testimonial = document.querySelector(".testimonial");
-        const slider = document.querySelector(".slider");
-        const features = document.querySelector(".features");
-        const carousel = document.querySelector(".carousel");
-        const team = document.querySelector(".team");
-
-        const io = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-
-                if (entry.target === header) {
-                    // console.log('header', entry.intersectionRatio);
-                }
-                if (entry.target === testimonial && entry.intersectionRatio > 0.4) {
-                    // console.log('testimonial', entry.intersectionRatio);
-                    testimonial.classList.add("active");
-                }
-                if (entry.target === previewCardsSection && entry.intersectionRatio > 0.4) {
-                    // console.log('previewCards', entry.intersectionRatio);
-                    previewCardsSection.classList.add("active");
-                }
-                if (entry.target === slider && entry.intersectionRatio > 0.4) {
-                    slider.classList.add("active");
-                }
-                if (entry.target === features && entry.intersectionRatio > 0.4) {
-                    features.classList.add("active");
-                }
-                if (entry.target === carousel && entry.intersectionRatio > 0.4) {
-                    carousel.classList.add("active");
-                }
-                if (entry.target === team && entry.intersectionRatio > 0.4) {
-                    team.classList.add("active");
-                }
-                if (entry.target === team && entry.intersectionRatio > 0.4) {
-                    team.classList.add("active");
-                }
-            });
-        }, {
-            threshold: [0.1, 0.5, 0.9]
-        });
-        io.observe(target);
-    };
-    document.querySelectorAll("[data-observer]").forEach(menuToggler); // data-observer
+        if (entry.target === header) {
+          // console.log('header', entry.intersectionRatio);
+        }
+        if (entry.target === testimonial && entry.intersectionRatio > 0.4) {
+          // console.log('testimonial', entry.intersectionRatio);
+          testimonial.classList.add("active");
+        }
+        if (entry.target === previewCardsSection && entry.intersectionRatio > 0.4) {
+          // console.log('previewCards', entry.intersectionRatio);
+          previewCardsSection.classList.add("active");
+        }
+        if (entry.target === slider && entry.intersectionRatio > 0.4) {
+          slider.classList.add("active");
+        }
+        if (entry.target === features && entry.intersectionRatio > 0.4) {
+          features.classList.add("active");
+        }
+        if (entry.target === carousel && entry.intersectionRatio > 0.4) {
+          carousel.classList.add("active");
+        }
+        if (entry.target === team && entry.intersectionRatio > 0.4) {
+          team.classList.add("active");
+        }
+        if (entry.target === team && entry.intersectionRatio > 0.4) {
+          team.classList.add("active");
+        }
+      });
+    }, {
+      threshold: [0.1, 0.5, 0.9]
+    });
+    io.observe(target);
+  };
+  document.querySelectorAll("[data-observer]").forEach(menuToggler); // data-observer
 
 
 })
